b77151ccfd59fb91fb9ba8ab6c7a91073f3995b2
Harshal Savaliya <hetsavaliya19@gmail.com>
2026-05-29 10:29:42 +0530

    Understanding @Primary annotation

--- Spring Boot/Spring-boot-learning/src/main/java/com/spring/core/Client.java
+++ Spring Boot/Spring-boot-learning/src/main/java/com/spring/core/Client.java
@@ -7,11 +7,6 @@ public class Client {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        Car car = applicationContext.getBean(Car.class);
-        Bike bike = applicationContext.getBean(Bike.class);
-        car.move();
-        bike.move();
-
         Traveller traveller = applicationContext.getBean(Traveller.class);
         traveller.startJourney();
     }
