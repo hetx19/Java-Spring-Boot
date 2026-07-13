0e9ad969f47dd2f3af82abed3a29b0f310ddf1f4
Harshal Savaliya <hetsavaliya19@gmail.com>
2026-07-13 19:37:11 +0530

    Created API's for create, update and delete Student using spring boot

--- Spring Boot/springboot-rest-api/src/main/java/net/java/spring/boot/controllers/StudentController.java
+++ Spring Boot/springboot-rest-api/src/main/java/net/java/spring/boot/controllers/StudentController.java
@@ -1,16 +1,15 @@
 package net.java.spring.boot.controllers;
 
 import net.java.spring.boot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class StudentController {
+    // GET Methods
 
     // http://localhost:8080/student
     @GetMapping("student")
@@ -47,5 +46,36 @@ public class StudentController {
     @GetMapping("students/query")
     public Student getStudentByRollNumber(@RequestParam("id") String rollNumber, @RequestParam("firstname") String firstName, @RequestParam("lastname") String lastName) {
         return new Student(rollNumber, firstName, lastName);
+    }
+
+    // POST Methods
+    // http://localhost:8080/students/create
+
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getRollNumber());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+
+        return student;
+    }
+
+    // PUT Methods
+    // http://localhost:8080/students/update/1
+    @PutMapping("students/update/{id}")
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") String rollNumber) {
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+
+        return student;
+    }
+
+    // DELETE Methods
+    // http://localhost:8080/students/delete/1
+    @DeleteMapping("students/delete/{id}")
+    public String deleteStudent(@PathVariable("id") String rollNumber) {
+        System.out.println(rollNumber);
+        return "Student deleted Successfully";
     }
 }
